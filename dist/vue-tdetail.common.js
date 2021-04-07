26fc2af463796cc8c73dc4748ffe49abcb92f362
yaodebian <yaodebian@gmail.com>
2021-04-07 19:38:55 +0800

    add notes of row.js

--- dist/vue-tdetail.common.js
+++ dist/vue-tdetail.common.js
@@ -4431,27 +4431,30 @@ function _slicedToArray(arr, i) {
     var len = 0;
 
     for (var n = i; n < i + col; n++) {
-      if (n >= list.length) {
+      // add rest space if n is equal with the list's length
+      if (n === list.length) {
         temp[temp.length - 1].colspan += col - len;
         i = n + 1;
         break;
-      }
+      } // make a deep copy before operation
+
 
       var tempItem = JSON.parse(JSON.stringify(list[n]));
-      tempItem.colspan = tempItem.colspan ? tempItem.colspan : 1;
+      tempItem.colspan = tempItem.colspan ? tempItem.colspan : 1; // compute rest space if we can't fill the "col"
 
       if (len + tempItem.colspan > col || len + tempItem.colspan <= col && n === i + col - 1) {
         tempItem.colspan = col - len;
       }
 
       temp.push(tempItem);
-      len += tempItem.colspan;
+      len += tempItem.colspan; // jump out of the loop if the "col" has filled
 
       if (len === col) {
         i = n + 1;
         break;
       }
-    }
+    } // push the array of a row
+
 
     resArr.push(temp);
   }
--- src/utils/rows.js
+++ src/utils/rows.js
@@ -7,23 +7,28 @@ export default (col, list) => {
     const temp = []
     let len = 0
     for (let n = i; n < i + col; n++) {
-      if (n >= list.length) {
+      // add rest space if n is equal with the list's length
+      if (n === list.length) {
         temp[temp.length - 1].colspan += col - len
         i = n + 1
         break
       }
+      // make a deep copy before operation
       const tempItem = JSON.parse(JSON.stringify(list[n]))
       tempItem.colspan = tempItem.colspan ? tempItem.colspan : 1
+      // compute rest space if we can't fill the "col"
       if ((len + tempItem.colspan > col) || (len + tempItem.colspan <= col && n === i + col - 1)) {
         tempItem.colspan = col - len
       }
       temp.push(tempItem)
       len += tempItem.colspan
+      // jump out of the loop if the "col" has filled
       if (len === col) {
         i = n + 1
         break
       }
     }
+    // push the array of a row
     resArr.push(temp)
   }
   return resArr
